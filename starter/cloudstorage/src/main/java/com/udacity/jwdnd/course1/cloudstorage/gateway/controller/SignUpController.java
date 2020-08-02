cc63dc67732905a08cf51b924896bb4848bee93a
dprio <daniel.pedroso94@gmail.com>
2020-08-02 12:01:11 -0300

    Logout and atutomatic redirect to login page

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/gateway/controller/SignUpController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/gateway/controller/SignUpController.java
@@ -3,6 +3,7 @@ package com.udacity.jwdnd.course1.cloudstorage.gateway.controller;
 import com.udacity.jwdnd.course1.cloudstorage.gateway.controller.request.SignUpRequest;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +24,15 @@ public class SignUpController {
     }
 
     @PostMapping
-    public String signUpUser(final SignUpRequest signUpRequest){
-        userService.createUser(signUpRequest.toUserDomain());
-        return "login";
+    public String signUpUser(final SignUpRequest signUpRequest, final Model model){
+        try {
+            userService.createUser(signUpRequest.toUserDomain());
+            model.addAttribute("signupSuccess", true);
+            return "signup";
+        }catch (final Exception e){
+            model.addAttribute("signupError", true);
+            return "signup";
+        }
     }
 
 }
